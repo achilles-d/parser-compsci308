9d13584e5a74ddec71242f09f5e14b01e2b776c0
Saurav Sanjay <sks61@duke.edu>
2020-02-20 10:10:15 -0500

    Added titled panes

--- src/slogo/view/LogoVisualization.java
+++ src/slogo/view/LogoVisualization.java
@@ -5,9 +5,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -22,9 +20,15 @@ public class LogoVisualization extends Application {
     {
 
         TextArea commandWindow = new TextArea();
+
+        TitledPane variablesWhole = new TitledPane();
+        variablesWhole.setText("Variables");
+        variablesWhole.setCollapsible(false);
+        variablesWhole.setPrefHeight(200);
         ListView<String> variableTracker =  new ListView<>();
         variableTracker.setPrefHeight(100);
-        variableTracker.getItems().addAll("varXSSSSSSSSSSSJFL:DKJF","varY","varY","varY","varY","varY","varY","varY","varY");
+        variableTracker.getItems().addAll("varXSSSSSSSSSSJFL:DKJF","varY","varY","varY","varY","varY","varY","varY","varY");
+        variablesWhole.setContent(variableTracker);
         commandWindow.setPrefWidth(650);
         commandWindow.setMaxWidth(Double.MAX_VALUE);
         Button execute = new Button("Execute");
@@ -34,8 +38,34 @@ public class LogoVisualization extends Application {
         Button reset = new Button("Reset");
         reset.setOnAction((event -> {commandWindow.clear();}));
 
+        TitledPane history = new TitledPane();
+        history.setText("Command History");
         ListView<String> commandHistory =  new ListView<>();
         commandHistory.getItems().addAll("fd 50","back 50", "loop 20");
+        history.setContent(commandHistory);
+        history.setCollapsible(false);
+
+        TitledPane available = new TitledPane();
+        available.setText("Available Commands");
+        ListView<String> availableCommands =  new ListView<>();
+        availableCommands.getItems().addAll("forward","backward","right","left");
+        available.setContent(availableCommands);
+        available.setExpanded(false);
+
+        VBox leftComps = new VBox();
+        leftComps.getChildren().addAll(history,available);
+
+        HBox menu = new HBox();
+        MenuItem itemBlue = new MenuItem("Blue");
+        MenuItem itemGreen = new MenuItem("Green");
+        MenuButton colors = new MenuButton("Colors",null,itemBlue,itemGreen);
+
+        MenuItem itemGerman = new MenuItem("German");
+        MenuItem itemSpanish = new MenuItem("Spanish");
+        MenuItem itemEnglish = new MenuItem("English");
+        MenuButton language = new MenuButton("Language",null,itemGerman,itemSpanish,itemEnglish);
+
+        menu.getChildren().addAll(colors,language);
 
         StackPane allTurtle = new StackPane();
         Image turtle = new Image("turtle.jpg");
@@ -50,12 +80,15 @@ public class LogoVisualization extends Application {
         buttons.getChildren().addAll(execute,reset);
         buttons.setAlignment(Pos.CENTER);
         //bottom.setPrefWidth(1000);
-        bottom.getChildren().addAll(variableTracker,commandWindow,buttons);
+        bottom.getChildren().addAll(variablesWhole,commandWindow,buttons);
         //HBox.setHgrow(border, Priority.ALWAYS);
         //border.setTop(filler);
+        allTurtle.setStyle("-fx-background-color: red");
         border.setBottom(bottom);
         border.setCenter(allTurtle);
-        border.setLeft(commandHistory);
+        border.setLeft(leftComps);
+        border.setTop(menu);
+       // border.setRight(available);
         turtleWrap.setTranslateX(200);
         //getChildren().addAll(border,allTurtle);
         //border.setPrefSize(900,900);
