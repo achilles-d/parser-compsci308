f66fa0c661c07707e1ece9bc86455f782939f1fa
Saurav Sanjay <sks61@duke.edu>
2020-02-20 21:30:50 -0500

    made coordinate class and turtle class

--- src/slogo/view/LogoVisualization.java
+++ src/slogo/view/LogoVisualization.java
@@ -30,124 +30,30 @@ public class LogoVisualization {
     public void init()
     {
 
-        /*
-        TextArea commandWindow = new TextArea();
-        commandWindow.setPrefWidth(650);
-        commandWindow.setMaxWidth(Double.MAX_VALUE);
-        Button execute = new Button("Execute");
-        execute.setOnAction((event -> {
-            System.out.println(commandWindow.getText());
-        }));
-        Button reset = new Button("Reset");
-        reset.setOnAction((event -> {commandWindow.clear();}));
-        */
-
         ConsoleWindow commandWindow = new ConsoleWindow();
-        /*
-        TitledPane variablesWhole = new TitledPane();
-        variablesWhole.setText("Variables");
-        variablesWhole.setCollapsible(false);
-        variablesWhole.setPrefHeight(200);
-        ListView<String> variableTracker =  new ListView<>();
-        variableTracker.setPrefHeight(100);
-        variableTracker.getItems().addAll("varXSSSSSSSSSSJFL:DKJF","varY","varY","varY","varY","varY","varY","varY","varY");
-        variablesWhole.setContent(variableTracker);
-        */
-
         VariableWindow myVariables = new VariableWindow();
-
-
-
-        /*
-        TitledPane history = new TitledPane();
-        history.setText("Command History");
-        ListView<String> commandHistory =  new ListView<>();
-        commandHistory.getItems().addAll("fd 50","back 50", "loop 20");
-        history.setContent(commandHistory);
-        history.setCollapsible(false);
-         */
-
         HistoryWindow myHistory = new HistoryWindow();
-
-        /*
-        TitledPane available = new TitledPane();
-        available.setText("Available Commands");
-        ListView<String> availableCommands =  new ListView<>();
-        availableCommands.getItems().addAll("forward","backward","right","left");
-        available.setContent(availableCommands);
-        available.setExpanded(false);
-        */
-
         AvailableCommandsWindow available = new AvailableCommandsWindow("resources.languages.English");
+        TurtleWindow graphics = new TurtleWindow();
+        Menu toolbar = new Menu();
 
         VBox leftComps = new VBox();
         leftComps.getChildren().addAll(myHistory.getView(),available.getView());
 
-
-        /*
-        HBox menu = new HBox();
-        MenuItem itemBlue = new MenuItem("Blue");
-        MenuItem itemGreen = new MenuItem("Green");
-        MenuButton colors = new MenuButton("Colors",null,itemBlue,itemGreen);
-
-        MenuItem itemGerman = new MenuItem("German");
-        MenuItem itemSpanish = new MenuItem("Spanish");
-        MenuItem itemEnglish = new MenuItem("English");
-        MenuButton language = new MenuButton("Language",null,itemGerman,itemSpanish,itemEnglish);
-
-        Button help = new Button("Help");
-        help.setOnAction(event -> {
-            Stage stage1 = new Stage();
-            Label helpText = new Label("This is a help screen");
-            Group helpGroup = new Group();
-            helpGroup.getChildren().addAll(helpText);
-            Scene helpScreen = new Scene(helpGroup,400,400);
-            stage1.setScene(helpScreen);
-            stage1.show();});
-
-        menu.getChildren().addAll(colors,language,help);
-        */
-
-        Menu toolbar = new Menu();
-        /*
-        StackPane allTurtle = new StackPane();
-        Image turtle = new Image("turtle.jpg");
-        ImageView turtleWrap = new ImageView(turtle);
-        Canvas turtleWindow = new Canvas();
-        allTurtle.getChildren().addAll(turtleWindow,turtleWrap);
-        turtleWrap.setFitHeight(50);
-        turtleWrap.setFitWidth(50);
-        */
-
-        TurtleWindow graphics = new TurtleWindow();
-
         HBox bottom  = new HBox();
-
-
-        //bottom.setPrefWidth(1000);
         bottom.getChildren().addAll(myVariables.getView(),commandWindow.getView());
 
-        //HBox.setHgrow(border, Priority.ALWAYS);
-        //border.setTop(filler);
-        //allTurtle.setStyle("-fx-background-color: red");
 
         border.setBottom(bottom);
         border.setCenter(graphics.getView());
         border.setLeft(leftComps);
         border.setTop(toolbar.getView());
-       // border.setRight(available);
-        //turtleWrap.setTranslateX(200);
-        //getChildren().addAll(border,allTurtle);
-        //border.setPrefSize(900,900);
 
-        //root.getChildren().addAll(border,turtleWrap);
-        //root.prefHeightProperty().bind(border.heightProperty());
-        //root.prefWidthProperty().bind(border.widthProperty());
 
         Scene scene = new Scene(border,1000,1000);
         myStage.setScene(scene);
         myStage.show();
-        //System.out.println(turtleWrap.getX());
+
 
     }
 
--- src/slogo/view/ViewTurtle.java
+++ src/slogo/view/ViewTurtle.java
@@ -3,11 +3,13 @@ package slogo.view;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import slogo.model.Coordinate;
 
 public class ViewTurtle {
 
     private Image myImage;
     private ImageView myView;
+    private Coordinate myCoordinates;
 
     public ViewTurtle()
     {
@@ -15,11 +17,17 @@ public class ViewTurtle {
         myView = new ImageView(myImage);
         myView.setFitWidth(50);
         myView.setFitHeight(50);
+        myCoordinates = new Coordinate();
     }
 
     public Node getView()
     {
         return myView;
+    }
+
+    public void updatePosition(Coordinate updatedCord)
+    {
+        myCoordinates = updatedCord;
     }
 
 }
