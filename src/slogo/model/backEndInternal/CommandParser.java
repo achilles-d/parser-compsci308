86ad2956eeeaaa5a33f78d1f1229e420f3b729f3
Abebe Amare <ama100@duke.edu>
2020-02-22 17:53:09 -0500

    Cleaning up the parser class

--- src/slogo/model/backEndInternal/CommandParser.java
+++ src/slogo/model/backEndInternal/CommandParser.java
@@ -10,46 +10,73 @@ import java.util.regex.Pattern;
 public class CommandParser implements Parser {
 
     private static final String RESOURCES_PACKAGE = CommandParser.class.getPackageName() + ".resources.languages.";
-   // private static final String NUMBER_OF_INPUT = CommandParser.class.getPackageName() + ".ArgumentS.languages.";
+
     private ResourceBundle sizes = ResourceBundle.getBundle(RESOURCES_PACKAGE + "ArgumentSize");
     private List<Map.Entry<String, Pattern>> mySymbols;
     private Stack<Double> argumentStack=new Stack<>();
     private Stack<String> commandStack=new Stack<>();
-    private List<String> commandFraction=new ArrayList<>();
-    private Map<String, Integer> numberOfInputs=new HashMap<>();
-    private int c=0;
-    //private CommandExecutor executor=new CommandExecutor();
+    private List<String> commandList=new ArrayList<>();
+    private Map<String, Runnable> matchMethodsToRun;
+    private int commandCounter=0;
 
     /**
      * Create an empty parser
      */
+
     public CommandParser () {
 
         mySymbols = new ArrayList<>();
+        matchMethodsToRun=new HashMap<>();
+        //String str;
+        matchMethodsToRun.put("Constant", this::parseConstant);
+        matchMethodsToRun.put("Command", this::parseCommand);
+        matchMethodsToRun.put("Variable", this::parseVariable);
+        matchMethodsToRun.put("ListStart", this::parseList);
+        matchMethodsToRun.put("GroupStart", this::parseGroup);
+       // matchMethodsToRun.put("Constant", () ->addToStack());
+    }
+
+    private void parseGroup() {
+        System.out.println("Working");
+    }
+
+    private void parseList() {
+        System.out.println("Working");
+    }
+
+    private void parseVariable() {
+    }
+
+    private void parseCommand() {
+        commandStack.add(commandList.get(commandCounter));
+        addValidNumToTheStack(commandList);
+    }
+
+    private void parseConstant() {
+        argumentStack.add(Double.parseDouble(commandList.get(commandCounter)));
     }
 
     @Override
     public void parseCode(String consoleInput) throws InvalidCommandException {
-        commandFraction.addAll(Arrays.asList(consoleInput.split(" ")));
+        commandList.addAll(Arrays.asList(consoleInput.split(" ")));
 
-        while(c<commandFraction.size()){
-            if(getSymbol(commandFraction.get(c)).equals("Constant")){
-                argumentStack.add(Double.parseDouble(commandFraction.get(c)));
+
+        while(commandCounter<commandList.size()){
+
+            if(!matchMethodsToRun.containsKey(getSymbol(commandList.get(commandCounter)))){
+                matchMethodsToRun.get("Command").run();
             } else{
-                commandStack.add(commandFraction.get(c));
-                isThereValidNum(commandFraction);
+                matchMethodsToRun.get(getSymbol(commandList.get(commandCounter))).run();
             }
 
-
             buildExecutable();
-            c++;
+            commandCounter++;
 
         }
-    //System.out.println("The size of commandStack is "+commandStack.size());
+
         int size=commandStack.size();
         for(int i=0;i<size;i++){
             buildExecutable();
-            System.out.println("The size of commandStack is "+commandStack.size());
         }
 
         }
@@ -59,38 +86,39 @@ public class CommandParser implements Parser {
 
             double x=0;
             int l=readArgumentSize(getSymbol(commandStack.peek()));
-            System.out.println("argument size need is "+l);
+            //System.out.println("argument size need is "+l);
             String st=commandStack.pop()+":";
-           // System.out.println(l);
+
             for( int j=0;j<l;j++){
                 st=st+" "+argumentStack.peek();
                 x+=argumentStack.pop();
             }
-            //System.out.println()
-            System.out.println("answer is "+st);
+
+            System.out.println(""+st);
             argumentStack.add(x);
-            System.out.println("summation is "+x);
+           // System.out.println("summation is "+x);
 
         }
 
     }
 
-    private void isThereValidNum(List<String> commandFraction) {
+    private void addValidNumToTheStack(List<String> commandFraction) {
         int count=0;
-        System.out.println("C values "+c);
-        for(int k=c+1; k<c+1+readArgumentSize(getSymbol(commandStack.peek()));k++){
+        //System.out.println("C values "+commandCounter);
+        for(int k=commandCounter+1; k<commandCounter+1+readArgumentSize(getSymbol(commandStack.peek()));k++){
             if(getSymbol(commandFraction.get(k)).equals("Constant")){
                 count++;
             }
         }
-        System.out.println("counting values "+count);
+
+        //System.out.println("counting values "+count);
         if(count==readArgumentSize(getSymbol(commandStack.peek()))){
-            for(int k=c+1; k<c+1+readArgumentSize(getSymbol(commandStack.peek()));k++){
+            for(int k=commandCounter+1; k<commandCounter+1+readArgumentSize(getSymbol(commandStack.peek()));k++){
                argumentStack.add(Double.parseDouble(commandFraction.get(k)));
-               System.out.println("Added "+commandFraction.get(k));
+               //System.out.println("Added "+commandFraction.get(k));
             }
-            c=c+readArgumentSize(getSymbol(commandStack.peek()));
-            System.out.println("C values "+c);
+            commandCounter=commandCounter+readArgumentSize(getSymbol(commandStack.peek()));
+            //System.out.println("C values "+commandCounter);
         }
 
 
@@ -102,37 +130,30 @@ public class CommandParser implements Parser {
         return null;
     }
 
-//    // check if we have enough argument for the command
-//    private boolean areArgumentsEnough(){
-//
-//        return (argumentStack.size()>=numberOfInputs.get(commandStack.peek()));
-//
-//    }
-
     /**
-     * Adds the given resource file to this language's recognized types
+     *
+     * @param syntax the name of the syntax source language name
+     *Adds the keys to mySymbols, thus comparison will be done
      */
     public void addPatterns (String syntax) {
         ResourceBundle resources = ResourceBundle.getBundle(RESOURCES_PACKAGE + syntax);
         for (String key : Collections.list(resources.getKeys())) {
             String regex = resources.getString(key);
             mySymbols.add(new AbstractMap.SimpleEntry<>(key,
-                    // THIS IS THE IMPORTANT LINE
                     Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
-
-           //System.out.println("Used key "+key);
-
         }
     }
 
+
     /**
-     * Returns language's type associated with the given text if one exists
+     *
+     * @param command the commands as a text
+     * @return the commands type
      */
-    public String getSymbol (String text) {
+    public String getSymbol (String command) {
         final String ERROR = "NO MATCH";
         for (Map.Entry<String, Pattern> e : mySymbols) {
-            if (match(text, e.getValue())) {
-              // System.out.println("getSymbol method "+e.getKey());
+            if (match(command, e.getValue())) {
                 return e.getKey();
             }
         }
@@ -146,9 +167,6 @@ public class CommandParser implements Parser {
     }
 
     private int readArgumentSize(String key){
-       // System.out.println(key);
-        //System.out.println(Integer.parseInt(sizes.getString(key)));
-
         return Integer.parseInt(sizes.getString(key));
 
     }
