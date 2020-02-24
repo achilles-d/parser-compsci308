650edc28c8705325ab359739e991312e91d9be8b
Achintya Kumar <ask60@duke.edu>
2020-02-24 18:56:14 -0500

    command to make user variable

--- src/slogo/model/backEndInternal/commands/MakeVariable.java
+++ src/slogo/model/backEndInternal/commands/MakeVariable.java
@@ -1,5 +1,18 @@
 package slogo.model.backEndInternal.commands;
 
-public class MakeVariable {
+import slogo.model.backEndInternal.UserVariable;
 
+public class MakeVariable<T> implements Command<UserVariable<T>> {
+
+  UserVariable<T> newVar;
+
+  public MakeVariable(T userVal) {
+    this.newVar = new UserVariable<>();
+    newVar.setValue(userVal);
+  }
+
+  @Override
+  public UserVariable<T> execute() {
+    return newVar;
+  }
 }
