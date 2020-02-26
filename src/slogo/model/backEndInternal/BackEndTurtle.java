e4027ced50a4e6bb272d7de355b31739b10a5f8c
Saurav Sanjay <sks61@duke.edu>
2020-02-26 17:15:31 -0500

    troubleshooting turtle hitting edge

--- src/slogo/model/backEndInternal/BackEndTurtle.java
+++ src/slogo/model/backEndInternal/BackEndTurtle.java
@@ -29,6 +29,8 @@ public class BackEndTurtle implements Turtle {
     private SimpleBooleanProperty turtleVisible = new SimpleBooleanProperty();
 
 
+
+
     //private double xLoc;
     //private double yLoc;
     //private double endX;
@@ -59,6 +61,8 @@ public class BackEndTurtle implements Turtle {
     public void setPosition(Coordinate a) {
 
         Coordinate newCord = ensureInBounds(a);
+        System.out.println("YAFTERADJUST " + newCord.getYVal());
+        System.out.println("XAFTERADJUST" + newCord.getXVal());
         if(!penUp.getValue())
         {
             drawLine(turtleCoordinate,newCord);
@@ -70,26 +74,42 @@ public class BackEndTurtle implements Turtle {
 
     }
 
-    private Coordinate ensureInBounds(Coordinate a)
-    {
-        double x =a.getXVal();
+    private Coordinate ensureInBounds(Coordinate a) {
+
+        double x = a.getXVal();
         double y = a.getYVal();
-        System.out.println("Y COORDINATE" + y);
 
-        while(x>350)
+        boolean hitXWall = false;
+        boolean hitYWall = false;
+
+
+        while (x > 370) {
             x--;
-        while(y>809.5)
-            y--;
-
-        while(x<-375)
+            hitXWall = true;
+        }
+        while (x < -370) {
             x++;
-        while(y<-286.5)
+            hitXWall = true;
+        }
+
+        while (y > 280){
+            y--;
+            hitYWall = true;
+
+        }
+
+        while (y < -280){
             y++;
+            hitYWall = true;
+
+        }
 
 
-        return new Coordinate(x,y);
+        return new Coordinate((!hitXWall && hitYWall? turtleCoordinate.getXVal():x),(hitXWall && !hitYWall? turtleCoordinate.getYVal():y));
 
     }
+
+
 
     @Override
     public Coordinate getPosition() {
