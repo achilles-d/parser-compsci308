57cb292507484ed2c4ca8e7cf89c23dfce3fdb16
Achintya Kumar <ask60@duke.edu>
2020-02-27 00:21:00 -0500

    multiline command inputs work

---
+++ src/slogo/model/backEndInternal/commands/To.java
@@ -0,0 +1,23 @@
+package slogo.model.backEndInternal.commands;
+
+import java.util.List;
+
+public class To implements Command<Double> {
+
+
+
+    @Override
+    public Double execute() {
+        return null;
+    }
+
+    @Override
+    public List<String> updateRawCommands() {
+        return null;
+    }
+
+    @Override
+    public Integer updateCounter() {
+        return null;
+    }
+}
