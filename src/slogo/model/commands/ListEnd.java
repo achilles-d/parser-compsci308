43717f806bfed21c5db7feeb0a407f929f254614
Abebe Amare <ama100@duke.edu>
2020-03-06 16:32:16 -0500

    refactored make variable

--- src/slogo/model/commands/ListEnd.java
+++ src/slogo/model/commands/ListEnd.java
@@ -7,14 +7,11 @@ public class ListEnd implements Command<Object> {
     List<String> group;
 
     public ListEnd(List<String> group){
-        System.out.println(" It should come here ");
-
         this.group=group;
     }
 
     @Override
     public List<String> execute() {
-        System.out.println(" it comes here in the command "+group.toString());
         return group;
     }
 
